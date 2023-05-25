40dcb777b0a4b6939a1848f8f1884137ab0e14aa
Chelsea <markhamonca@codeninjas.com>
2023-05-25 19:00:41 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
    HELP ME

--- things that do not look like asteroids/Assets/Scripts/Player.cs
+++ things that do not look like asteroids/Assets/Scripts/Player.cs
@@ -4,32 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //make appear                     //Vector3   rotation
-            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {                                  //does not matter how fast the computer speed is at
-            transform.Translate(Vector2.up * Time.deltaTime * speed);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector2.down * Time.deltaTime * speed);
-        }
+        
     }
 }
